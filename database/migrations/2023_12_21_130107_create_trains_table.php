1fcb8baf2fec6e1007449018d2cc4457568b9bdb
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-12-21 15:19:30 +0100

    added trains trable

---
+++ database/migrations/2023_12_21_130107_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 255);
+            $table->string('stazione_partenza', 255);
+            $table->string('stazione_arrivo', 255);
+            $table->date('orario_partenza');
+            $table->date('orario_arrivo');
+            $table->string('codice_treno', 12)->unique();
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->tinyInteger('in_orario')->default(1);
+            $table->tinyInteger('cancellato')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
